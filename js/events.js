652a53b98af3ed012437040bf3a0a30b29307d35
William Damm <wdamm@powerhrg.com>
2020-11-17 17:49:33 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,14 +1,14 @@
 //define functions here
 
-
-
 function getIt () {
   $('p').on('click', function() {
     alert("Hey!");
   });
 }
 
-
+function frameIt () {
+  
+}
 
 
 $(document).ready(function(){
