1f80e8da604f15a8790cc73a91af0bfdac65fb00
William Damm <wdamm@powerhrg.com>
2020-11-17 18:04:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt () {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
 
-// call functions here
+function frameIt () {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('youve pressed g');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+
+$(document).ready(function(){
 
 });
